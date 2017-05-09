4703218f299957ff61e77d224c661d9afeac24d4
Paul Upendo <Upendo@Pauls-MacBook-Pro.local>
2017-05-09 15:33:00 +0300

    Import app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,14 @@
-from flask import flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from instance.config import app_config
 
 app = Flask(__name__)
+databases = SQLAlchemy(app)
+
+
+def EnvironmentName(environ):
+    app.config.from_object(app_config[environ])
+
+
+EnvironmentName('TestingConfig')
