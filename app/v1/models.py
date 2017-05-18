2320e2a35533bb2e0b0e9e6186bd0bd308ca67d5
Paul Upendo <Upendo@Pauls-MacBook-Pro.local>
2017-05-18 11:18:45 +0300

    Reformat code to pep8 line line length style

--- app/v1/models.py
+++ app/v1/models.py
@@ -6,12 +6,16 @@ from passlib.apps import custom_app_context as pwd_context
 class Items(databases.Model):
     __tablename__ = 'BucketListItems'
 
-    id = databases.Column(databases.Integer, primary_key=True, autoincrement=True)
+    id = databases.Column(databases.Integer,
+                          primary_key=True, autoincrement=True)
     name = databases.Column(databases.String(250))
-    datecreated = databases.Column(databases.DateTime, default=datetime.utcnow())
-    date_modified = databases.Column(
-        databases.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
-    bucketlist_id = databases.Column(databases.Integer(), databases.ForeignKey('Bucketlist.id'))
+    datecreated = databases.Column(databases.DateTime,
+                                   default=datetime.utcnow())
+    date_modified = databases.Column(databases.DateTime,
+                                     default=datetime.utcnow(),
+                                     onupdate=datetime.utcnow())
+    bucketlist_id = databases.Column(databases.Integer(),
+                                     databases.ForeignKey('Bucketlist.id'))
 
     def __init__(self, name, id):
         self.name = name
@@ -27,13 +31,17 @@ class Items(databases.Model):
 
 class BucketList(databases.Model):
     __tablename__ = 'Bucketlist'
-    id = databases.Column(databases.Integer, primary_key=True, autoincrement=True)
-    user = databases.Column(databases.Integer(), databases.ForeignKey('UserInfo.id'))
+    id = databases.Column(databases.Integer,
+                          primary_key=True, autoincrement=True)
+    user = databases.Column(databases.Integer(),
+                            databases.ForeignKey('UserInfo.id'))
     name = databases.Column(databases.String(255))
     items = databases.relationship('Items', backref="Bucketlist")
-    date_created = databases.Column(databases.DateTime, default=datetime.utcnow())
+    date_created = databases.Column(databases.DateTime,
+                                    default=datetime.utcnow())
     date_modified = databases.Column(
-        databases.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+        databases.DateTime, default=datetime.utcnow(),
+        onupdate=datetime.utcnow())
     created_by = databases.Column(databases.Integer)
 
     def __init__(self, name):
@@ -46,7 +54,8 @@ class BucketList(databases.Model):
 
 class Users(databases.Model):
     __tablename__ = 'UserInfo'
-    id = databases.Column(databases.Integer, primary_key=True, autoincrement=True)
+    id = databases.Column(databases.Integer,
+                          primary_key=True, autoincrement=True)
     Bucket = databases.relationship('BucketList', backref='UserInfo')
     username = databases.Column(databases.String(250))
     password_hash = databases.Column(databases.String(300))
