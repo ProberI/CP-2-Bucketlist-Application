504eff611d0e41b06913ff8e2b7bea1d2ebff05d
ProberI <lovepaul46@outlook.com>
2017-05-22 13:06:58 +0300

    Feature: Create and delete db manually

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -8,9 +9,26 @@ from flask_migrate import Migrate, MigrateCommand
 from app.v1 import models
 from app import app, databases
 
+
 migrate = Migrate(app, databases)
 manager = Manager(app)
 manager.add_command('databases', MigrateCommand)
+
+
+@manager.command
+def init_db():
+    os.system('createdb bucketlist_db')
+    print('Databases created')
+
+
+@manager.command
+def drop_db():
+    os.system(
+        'psql -c "DROP DATABASE IF EXISTS test_db"')
+    os.system(
+        'psql -c "DROP DATABASE IF EXISTS bucketlist_db"')
+    print('Databases dropped')
+
 
 if __name__ == '__main__':
     manager.run()
