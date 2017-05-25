7d445b5a73bc37514cd84cc5a717a5b9fb1fc174
ProberI <lovepaul46@outlook.com>
2017-05-25 12:16:30 +0300

    heroku config

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 from app import app
 
-app.run()
+
+@app.route('/')
+def index():
+    return "Welcome to BucketList Application API"
+
+
+if __name__ == '__main__':
+    app.run()
